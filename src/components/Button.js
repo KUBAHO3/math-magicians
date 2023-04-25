c5e8be1b67773b4a7e30e521d9a1488e7a4e8903
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-25 17:05:37 +0530

    Make use of PropTypes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
 function Button({ title, cname }) {
   return (
     <button type="button" className={cname}>{title}</button>
   );
 }
-
+Button.defaultProps = {
+  cname: '',
+};
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  cname: PropTypes.string,
+};
 export default Button;
