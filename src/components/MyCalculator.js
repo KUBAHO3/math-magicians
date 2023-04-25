3c3ba20b810041949b9e4045dc7b9540fa864714
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-25 15:03:33 +0530

    Add button structure

--- src/components/MyCalculator.js
+++ src/components/MyCalculator.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function MyCalculator() {
   return (
-    <div>MyCalculator</div>
+    <div className="calc-wrapper">
+      <div className="calc-display">0</div>
+      <div className="calc-row">
+        <button type="button" className="cal-btn-normal">AC</button>
+        <button type="button" className="cal-btn-normal">+/-</button>
+        <button type="button" className="cal-btn-normal">%</button>
+        <button type="button" className="cal-btn-special">÷</button>
+      </div>
+    </div>
   );
 }
 
