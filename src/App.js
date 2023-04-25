01f155baa4f000534c983a821bcdffc4da821f2f
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-25 14:36:36 +0530

    Call MyCalcutor component in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MyCalculator from './components/MyCalculator';
 
 function App() {
   return (
     <div className="App">
       <h1>My Calculator</h1>
+      <MyCalculator />
     </div>
   );
 }
