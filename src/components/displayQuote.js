931c4c571d88d3a10e2953b0d714142f8076495d
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-26 19:40:30 +0530

    Define needed useStates

--- src/components/displayQuote.js
+++ src/components/displayQuote.js
@@ -3,6 +3,10 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 function DisplayQuote() {
+    const [myQuote, setMyQuote] = useState('');
+    const [quoteLoading, setQuoteLoading] = useState(true);
+    const [quoteError, setQuoteError] = useState(null);
+    
   return (
     <div>DisplayQuote</div>
   )
