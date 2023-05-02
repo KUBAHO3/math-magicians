8abe3c5af196b54175672467cdc2cc283163daa9
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-02 18:26:10 +0530

    Create Navbar for the app

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <h2 className='logo'>Math Magicians</h2>
+        <ul className='nav-item'>
+            <li className='nav-item-list'>Home</li>
+            <li className='nav-item-list'>Calculator</li>
+            <li className='nav-item-list'>Quote</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
