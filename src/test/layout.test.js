1025118f3a337341a32612eeee21b7399d013204
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-03 14:49:24 +0530

    Refine tests

--- src/test/layout.test.js
+++
@@ -1,9 +0,0 @@
-import renderer from 'react-test-renderer';
-import Layout from '../pages/Layout';
-
-it('renders Quote page correctly', () => {
-  const tree = renderer
-    .create(<Layout />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
